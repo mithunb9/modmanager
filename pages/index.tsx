b613691e0208224672d649e7559cfa1f1c0d3710
sendmithunb@gmail.com <sendmithunb@gmail.com>
2021-08-01 23:04:33 -0500

    add navbar ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,51 @@
 import { signIn, signOut, useSession } from "next-auth/client";
-import Image from "next/image";
+import { Button, Image, Box, Flex, Text } from "@chakra-ui/react";
 
 export default function Home() {
   const [session, loading] = useSession();
 
   return (
     <>
-      {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={() => signIn()}>Sign in</button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session?.user?.name} <br />
-          <Image
-            src={session?.user?.image as string}
-            alt="Profile Picture"
-            width={500}
-            height={500}
-          />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )}
+      <Flex bg="#26547C" color="white" justify="space-between" p="3">
+        <Text fontSize="3xl">Mod Manager</Text>
+        <Box>
+          <Flex>
+            {!session && (
+              <>
+                <Button
+                  onClick={() => signIn()}
+                  color="white"
+                  borderColor="white"
+                  variant="outline"
+                >
+                  Sign in
+                </Button>
+              </>
+            )}
+            {session && (
+              <>
+                <Image
+                  src={session?.user?.image as string}
+                  alt="Profile Picture"
+                  borderRadius="full"
+                  boxSize="50px"
+                  mx="5"
+                />
+                <Text fontSize="lg">{session?.user?.name}</Text>
+                <Button
+                  onClick={() => signOut()}
+                  color="white"
+                  borderColor="white"
+                  variant="outline"
+                  mx="5"
+                >
+                  Sign out
+                </Button>
+              </>
+            )}
+          </Flex>
+        </Box>
+      </Flex>
     </>
   );
 }
