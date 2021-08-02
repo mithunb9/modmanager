114d5891d773198e90c6df768274f16fc4c67828
sendmithunb@gmail.com <sendmithunb@gmail.com>
2021-08-01 20:35:34 -0500

    implement basic authentication

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const firestore = (
+  firebase.apps[0] ?? firebase.initializeApp(config)
+).firestore();
+
+export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: FirebaseAdapter(firestore),
+});
